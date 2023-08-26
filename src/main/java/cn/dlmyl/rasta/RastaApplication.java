426e92b0ffbb1b31e467cf75286d1770a89febff
dlmyL <916748598@qq.com>
2023-08-26 19:21:01 +0800

    Initial commit

---
+++ src/main/java/cn/dlmyl/rasta/RastaApplication.java
@@ -0,0 +1,18 @@
+package cn.dlmyl.rasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 瑞斯塔-短链系统
+ *
+ * @author dlmyL
+ */
+@SpringBootApplication
+public class RastaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RastaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dlmyl/rasta/RastaApplicationTest.java
@@ -0,0 +1,19 @@
+package cn.dlmyl.rasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * RastaApplicationTest
+ *
+ * @author dlmyL
+ */
+@SpringBootTest
+public class RastaApplicationTest {
+
+    @Test
+    void test_Context() {
+
+    }
+
+}
